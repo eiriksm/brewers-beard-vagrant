21aea50053a11b54383b7b43a8781a7219656309
Eirik S. Morland <eirik@morland.no>
2014-02-04 20:14:03 +0100

    Add first start of this.

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+apt-get update
+
+# Make it possible to add repository.
+apt-get install python-software-properties -y
+
+# Install cutting edge node.js
+add-apt-repository ppa:chris-lea/node.js-devel
+
+# Refresh stuff.
+apt-get update
+
+# Install some stuff.
+apt-get install build-essential g++ vim git curl nodejs -y
+
