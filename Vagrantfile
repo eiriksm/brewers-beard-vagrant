9664536a89ff8a296e16f07ad5c2dfe1d02e7a3b
Eirik S. Morland <eirik@morland.no>
2014-05-14 18:10:09 +0200

    Use 14.04 + actually do the right thing on startup.

--- Vagrantfile
+++ Vagrantfile
@@ -5,9 +5,9 @@
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-    config.vm.box = "precise64"
-    config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+    config.vm.box = "ubuntu/trusty64"
     config.vm.network :forwarded_port, guest: 3000, host: 3000
+    config.vm.network :forwarded_port, guest: 5984, host: 5984
     config.vm.provision :shell, :path => "bootstrap.sh"
     config.vm.synced_folder "../brewers-beard", "/server"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -5,16 +5,29 @@ apt-get update
 # Make it possible to add repository.
 apt-get install python-software-properties -y
 
-# Install cutting edge node.js
-add-apt-repository ppa:chris-lea/node.js-devel -y
+# Add couchdb repo.
+add-apt-repository ppa:couchdb/stable -y
 
 # Refresh stuff.
 apt-get update
 
 # Install some stuff.
-apt-get install build-essential g++ vim git curl nodejs -y
+apt-get install build-essential g++ vim git curl couchdb -y
 
-# Just override node alias.
+## Install nvm.
+curl https://raw.githubusercontent.com/creationix/nvm/v0.7.0/install.sh | HOME=/home/vagrant sh
+
+# Load nvm.
+. "/home/vagrant/.nvm/nvm.sh"
+
+# Install .11
+nvm install 0.11
+
+# Just override node alias and add nvm preference.
+mv /home/vagrant/.profile /home/vagrant/.bash_profile
 echo "alias node='node --harmony'" >> /home/vagrant/.bash_profile
+echo "nvm use 0.11" >> /home/vagrant/.bash_profile
 
-#
+# Fix some permissions.
+chown vagrant:vagrant /home/vagrant/.bash_profile
+chown vagrant:vagrant /home/vagrant/.nvm
